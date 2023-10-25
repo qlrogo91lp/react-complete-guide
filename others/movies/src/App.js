7eedff378e2e4b83ddfddf77a3af76a7dfe9e983
yjkim <yjkim@4net.co.kr>
2023-10-25 14:28:08 +0900

    sending http request

---
+++ others/movies/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchMoviesHandler = useCallback(async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch("https://swapi.dev/api/films/");
+        
+        if (!response.ok) {
+          throw new Error('Something went wroong!');
+        }
+
+        const data = await response.json();
+        const transformedMovies = data.results.map(movieData => {
+          return {
+            id: movieData.epsode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseData: movieData.release_date,
+          }
+        });
+        setMovies(transformedMovies);
+        setIsLoading(false);
+
+      } catch(error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    });
+
+    useEffect(() => {
+      fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
+
+    let content = <p>Found no movies.</p>;
+    
+    if (movies.length > 0) {
+      content = <MoviesList movies={movies} />
+    }
+
+    if (error) {
+      content = <p>{}</p>;
+    }
+
+    if (isLoading) {
+      content = <p>Loading...</p>;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
+            {/* <section>
+                {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+                {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
+                {!isLoading && error && <p>{error}</p>}
+                {isLoading && <p>Loading...</p>}
+            </section> */}
+        </React.Fragment>
+    );
+}
+
+export default App;
