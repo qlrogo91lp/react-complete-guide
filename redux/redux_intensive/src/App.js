07cd950e3da93b2028131f6810be9163123abf29
yjkim <qlrogo91lp@gmail.com>
2023-11-14 14:47:46 +0900

    Redux final

--- redux/redux_intensive/src/App.js
+++ redux/redux_intensive/src/App.js
@@ -1,14 +1,124 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+
+let isInitial = true;
 
 function App() {
-  return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
-  );
+    const dispatch = useDispatch();
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    const notification = useSelector((state) => state.ui.notification);
+
+    // Only rendering once
+    useEffect(() => {
+      dispatch(fetchCartData());
+    }, [dispatch]); // dispatch will never change
+
+    // 이 방법은 component를 leaner하게 만든다
+    // 여러 작업을 하지 않는다
+    useEffect(() => {
+        if (isInitial) {
+          isInitial = false;
+          return;
+        }
+
+        if (cart.changed) {
+          dispatch(sendCartData(cart));
+        }
+
+    }, [cart, dispatch]); // dispatch will never change
+
+    return (
+        <Fragment>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
+    );
 }
 
 export default App;
+
+/*
+function App() {
+    const dispatch = useDispatch();
+    const showCart = useSelector((state) => state.ui.cartIsVisible);
+    const cart = useSelector((state) => state.cart);
+    const notification = useSelector((state) => state.ui.notification);
+
+    useEffect(() => {
+        // async 코드를 사용하려면 useEffect() 안에서 사용해야만 한다
+        const sendCartData = async () => {
+            dispatch(
+                uiActions.showNotification({
+                    status: "pending",
+                    title: "Sending...",
+                    message: "Sending cart data!",
+                })
+            );
+            const response = await fetch(
+                "https://react-http-4040a-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+                { method: "PUT", body: JSON.stringify(cart) }
+            );
+
+            if (!response.ok) {
+                throw new Error("Sending cart datat failed!");
+            }
+
+            if (isInitial) {
+              isInitial = false;
+              return;
+            }
+
+            dispatch(
+                uiActions.showNotification({
+                    status: "success",
+                    title: "Success!",
+                    message: "Sending cart data!",
+                })
+            );
+
+          };
+
+          sendCartData().catch((error) => {
+              dispatch(
+                  uiActions.showNotification({
+                      status: "error",
+                      title: "Error!",
+                      message: "Sending cart data failed!",
+                  })
+              );
+          });
+    }, [cart, dispatch]);
+
+    return (
+        <Fragment>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
+            <Layout>
+                {showCart && <Cart />}
+                <Products />
+            </Layout>
+        </Fragment>
+    );
+} 
+*/
---
+++ redux/redux_intensive/src/store/cart-slice.js
@@ -0,0 +1,107 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+        changed: false,
+    },
+    /**
+     * Reducer 함수 내에서는 비동기 코드를 절대 사용해서는 안됨
+     */
+    reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(
+                (item) => item.id === newItem.id
+            );
+            state.totalQuantity++;
+            state.changed = true;
+
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title,
+                });
+            } else {
+                existingItem.quantity = existingItem.quantity + 1;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            state.changed = true;
+
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        },
+    },
+});
+
+
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
+
+/*
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: [],
+        totalQuantity: 0,
+    },
+    reducers: {
+        replaceCart(state, action) {
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(
+                (item) => item.id === newItem.id
+            );
+            state.totalQuantity++;
+
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title,
+                });
+            } else {
+                existingItem.quantity = existingItem.quantity + 1;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        },
+    },
+});
+*/
